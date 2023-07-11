5ee7cfaff66f750335f2be97187c3aa48ab01492
Tamara Salinas <gatamara29@gmail.com>
2023-07-11 10:18:30 -0400

    details

--- src/components/transaction/TransactionItem.tsx
+++ src/components/transaction/TransactionItem.tsx
@@ -13,18 +13,21 @@ export const TransactionItem: React.FC<Props> = ({ transaction }: Props) => {
     const isNegative = transaction.amount.toString()[0] === '-'
 
     return (
-        <li className={`  text-white px-3 py-1 rounded-lg mb-2 w-full flex justify-between items-center ${isNegative ? 'bg-red-400' : 'bg-zinc-600'}`} >
+        <li className={`  text-white px-3 py-1 rounded-lg mb-2 w-full flex flex-row ${isNegative ? 'bg-red-400' : 'bg-zinc-600'}`} >
 
-            <p className="text-sm">{transaction.description} </p>
-
-            <strong className="right-0">{toCurrency(transaction.amount.toString())}  </strong>
-            <button className="py-1 bg-fuchsia-950 rounded-lg " onClick={() => {
-                if (deleteTransaction) { deleteTransaction(transaction.id) };
-            }}>
-                <Trash color="#DBDDDF" />
-            </button>
-
-
+            <div className="basis-3/6">
+                <p className="text-s">{transaction.description} </p>
+            </div>
+            <div className=" basis-2/6 flex flex-row-reverse">
+                <strong className="right-0">{toCurrency(transaction.amount.toString())}  </strong>
+            </div>
+            <div className="basis-1/6 flex flex-row-reverse items-center">
+                <button className="p-1 h-8  bg-fuchsia-950 rounded-lg " onClick={() => {
+                    if (deleteTransaction) { deleteTransaction(transaction.id) };
+                }}>
+                    <Trash color="#DBDDDF" />
+                </button>
+            </div>
         </li>
     )
 }
